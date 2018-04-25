d4373066dfbda1b4f13b3fbb1dac66eb4eec6d65
Rahul Nigam <rnigam@smu.edu>
2018-04-25 09:35:39 -0400

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team 
+      Rahul's New League of Avengers
       </div>
       <div class="col-lg-6">
       </div>
